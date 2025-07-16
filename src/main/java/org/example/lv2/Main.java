c9ee4bec84e5845780d7cd26ed86c168e1eaa998
eunji-0508 <cojia@naver.com>
2025-07-16 14:43:41 +0900

    add

--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -11,7 +11,12 @@ public class Main {
         MenuItem cheeseBurger = new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         MenuItem hamburger = new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거");
 
+        final List<MenuItem> menuItems = new ArrayList<>();
 
+        menuItems.add(shackBurger);
+        menuItems.add(smokeShack);
+        menuItems.add(cheeseBurger);
+        menuItems.add(hamburger);
 
 
     }
