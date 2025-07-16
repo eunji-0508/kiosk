62312da2b0251636dcf56850c55fd52560f749f9
eunji-0508 <cojia@naver.com>
2025-07-16 14:43:00 +0900

    Object

--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -1,5 +1,18 @@
 package org.example.lv2;
 // Lv 2. 객체 지향 설계를 적용해 햄버거 메뉴를 클래스로 관리하기
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        MenuItem shackBurger = new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem smokeShack = new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem cheeseBurger = new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem hamburger = new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거");
+
+
+
+
+    }
 }
--- src/main/java/org/example/lv2/MenuItem.java
+++ src/main/java/org/example/lv2/MenuItem.java
@@ -3,10 +3,15 @@ package org.example.lv2;
 public class MenuItem {
     // 속성(필드)
     private String name;
-    private int price;
+    private int price;      // BigDecimal 모르니까 int로...
     private String explanation;
 
     // 생성자
+    public MenuItem(String name, int price, String explanation) {
+        this.name = name;
+        this.price = price;
+        this.explanation = explanation;
+    }
 
     // 기능
 }
