47b4d11f684aa101879e2301aebddef207e5650e
eunji-0508 <cojia@naver.com>
2025-07-17 20:35:17 +0900

    error

--- src/main/java/org/example/lv4/Menu.java
+++ src/main/java/org/example/lv4/Menu.java
@@ -17,6 +17,15 @@ public class Menu {
     // 기능
     // 게터(Getter)
     // List에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    public void printMenuItems() {
+        String message1 = "%d. %-15s | W %4.1f | %s";
+        for (int i = 1; i <= 4; i++) {
+            String messageType = String.format(message1, i, menuItems.get(i - 1).getName(), menuItems.get(i - 1).getPrice(), menuItems.get(i - 1).getExplanation());
+            System.out.println(messageType);
+        }
+    }
+
+
     // List를 리턴하는 함수
     // for..? return..? 둘 다 동시에 만족시켜야 하나?
     public List<MenuItem> getMenuItems() {
