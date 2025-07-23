2990c9d4d8a1380f62ca15991dc44df3833f476b
eunji-0508 <cojia@naver.com>
2025-07-23 17:57:43 +0900

    error

--- src/main/java/org/example/lv6/Cart.java
+++ src/main/java/org/example/lv6/Cart.java
@@ -5,38 +5,19 @@ import java.util.List;
 
 public class Cart {
     // 속성(필드)
-    List<CartItem> cartItems;
+    CartService cartService;
 
     // 생성자
-    public Cart(List<CartItem> cartItems) {
-        this.cartItems = new ArrayList<>();
+    public Cart(CartService cartService) {
+        this.cartService = cartService;
     }
-
 
     // 기능(메서드)
-    // 메뉴 추가 기능
-    public void addItem(CartItem cartItem) {
-        cartItems.add(cartItem);
-    }
+    public void start() {
+        cartService.
+        System.out.println(" 위 메뉴를 장바구니에 추가하시겠습니까?");
 
-    // 장바구니 출력 기능
-    public void printCart() {
-        String message = "%d. %-15s | W %4.1f | %s";
-        for (int i = 0; i < cartItems.size(); i++) {
-            System.out.println(String.format(message, i, cartItems.get(i - 1).getName(), cartItems.get(i - 1).getPrice(), cartItems.get(i - 1).getExplanation()));
-        }
     }
-
-    // 장바구니 총 금액 계산 기능
-    public void totalPrice() {
-        double sum = 0;
-        for(CartItem cartItem : cartItems) {
-            sum += cartItem.getPrice();
-        }
-        System.out.println("[ Total ]");
-        System.out.println("W " + sum);
-    }
-
 
 
 }
---
+++ src/main/java/org/example/lv6/CartService.java
@@ -0,0 +1,38 @@
+package org.example.lv6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartService {
+    // 속성(필드)
+    List<CartItem> cartItems;
+
+    // 생성자
+    public CartService(List<CartItem> cartItems) {
+        this.cartItems = new ArrayList<>();
+    }
+
+    // 기능
+    // 메뉴 추가 기능
+    public void addItem(CartItem cartItem) {
+        cartItems.add(cartItem);
+    }
+
+    // 장바구니 출력 기능
+    public void printCart() {
+        String message = "%d. %-15s | W %4.1f | %s";
+        for (int i = 0; i < cartItems.size(); i++) {
+            System.out.println(String.format(message, i, cartItems.get(i - 1).getName(), cartItems.get(i - 1).getPrice(), cartItems.get(i - 1).getExplanation()));
+        }
+    }
+
+    // 장바구니 총 금액 계산 기능
+    public void totalPrice() {
+        double sum = 0;
+        for(CartItem cartItem : cartItems) {
+            sum += cartItem.getPrice();
+        }
+        System.out.println("[ Total ]");
+        System.out.println("W " + sum);
+    }
+}
